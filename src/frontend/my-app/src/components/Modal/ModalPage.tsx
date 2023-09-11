9c215b5e1016caf06824a47362bf8794e2c69690
Pedro Davi <pedro.henrique04896@gmail.com>
2023-09-10 23:11:04 -0300

    Update

--- src/frontend/my-app/src/components/Modal/ModalPage.tsx
+++ src/frontend/my-app/src/components/Modal/ModalPage.tsx
@@ -1,7 +1,8 @@
 
-import { useDisclosure, FormLabel } from "@chakra-ui/react";
+import { useDisclosure, FormLabel, Input, Textarea, Button } from "@chakra-ui/react";
 import React from "react";
 import { ModalGeneric } from "./Modal";
+import "./SolicitaEvidencia.css";
 
 export const ModalSolicitaEvidencia = () =>{
     const { isOpen, onOpen, onClose } = useDisclosure();
@@ -11,9 +12,17 @@ export const ModalSolicitaEvidencia = () =>{
             <>
             <button onClick={onOpen}>Pedir evidência</button>
                 <ModalGeneric isOpen={isOpen} onClose={onClose}>
-            <FormLabel>Solicitação de evidência</FormLabel>
+            <FormLabel><strong>Solicitação de evidência</strong></FormLabel>
+                <FormLabel id="label">Documento requerido</FormLabel>
+                <Input placeholder='Digite o documento requerido' size='md' />
 
+                <FormLabel id="label">Descrição</FormLabel>
+                <Textarea placeholder='Descreva a solicitação' />
 
+                <FormLabel id="label">Data de entrega</FormLabel>
+                <Input placeholder="Selecione a data" size="md" type="datetime-local"/>
+
+                <Button colorScheme='teal' variant='solid'>Enviar</Button>
         </ModalGeneric></>
         
     );
