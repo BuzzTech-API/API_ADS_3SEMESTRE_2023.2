5506ea0db7db8ba0c52330e2e2da6e64f8c8f3b3
Pedro Davi <pedro.henrique04896@gmail.com>
2023-09-10 22:32:56 -0300

    update

--- src/frontend/my-app/src/App.tsx
+++ src/frontend/my-app/src/App.tsx
@@ -5,19 +5,20 @@ import { Home } from './pages/Home'
 import { Login } from './pages/Login'
 import SideBar from './components/SideBar/SideBar'
 import { PageModal } from './pages/ModalPage'
+import { ModalSolicitaEvidencia } from './components/Modal/ModalPage'
 
 // rotas
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route path="/" element={<Home />}>
-      </Route>
-      <Route path="/modal" element={<PageModal />}>
-      </Route>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/modal" element={<PageModal />}></Route>
 
       <Route path='/login' element={<Login />}></Route>
-    </>
+      <Route path="/" element={<ModalSolicitaEvidencia/>}></Route>
 
+      
+    </>
   )
 )
 
