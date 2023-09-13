9c3f1ffccc1b2aabbb448469b9b69a4da2059e2c
NilberSiqueira <nilbernick7@gmail.com>
2023-09-13 11:29:04 -0300

    Interface Criação etapas

--- src/frontend/my-app/src/pages/Home.tsx
+++ src/frontend/my-app/src/pages/Home.tsx
@@ -1,8 +1,9 @@
 import React from "react"
 import SideBar from "../components/SideBar/SideBar"
+import EtapaForm from "../components/EtapaForm"
 
 export const Home = () => {
     return(
-        <SideBar></SideBar>
+        <EtapaForm></EtapaForm>
     )
 }
