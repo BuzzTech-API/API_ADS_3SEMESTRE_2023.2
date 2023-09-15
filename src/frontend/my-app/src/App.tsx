773f59d8c7c6d3c481d45f9684e3f8d454eb0f0d
IvanDuarte <ivangermanoduarte@gmail.com>
2023-09-14 22:20:33 -0300

    Modificações no Form

--- src/frontend/my-app/src/App.tsx
+++ src/frontend/my-app/src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react'; //Importação do React
-import { ChakraProvider, CSSReset, extendTheme } from '@chakra-ui/react'; //Importação da Biblioteca do Chakra
-import Client_Form from './components/form/ClientForm';
-import { Home } from "./pages/Home";
-
-import './App.css';
+import { ChakraProvider, CSSReset} from '@chakra-ui/react'; //Importação da Biblioteca do Chakra
+import ClientForm from './components/form/ClientForm';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import './App.css';
+
 
 function App() {
   return (
     <ChakraProvider>
       <CSSReset />
       <div className="App">
-        <Client_Form />
+        <ClientForm/>
       </div>
     </ChakraProvider>
   );
--- src/frontend/my-app/src/components/form/ClientForm.tsx
+++ src/frontend/my-app/src/components/form/ClientForm.tsx
@@ -1,16 +1,29 @@
 import React, { useState } from 'react';
-import { Box, Button, FormControl, FormLabel, Input, Checkbox, CheckboxGroup, CircularProgress, CircularProgressLabel  } from '@chakra-ui/react';
-import './ClientForm.css';
+import { cardAnatomy } from '@chakra-ui/anatomy'
+import { Box,
+         Button,
+         FormControl,
+         FormLabel,
+         Input,
+         Card, 
+         CardHeader, 
+         CardBody, 
+         Heading, 
+         createMultiStyleConfigHelpers, 
+         defineStyle } from '@chakra-ui/react';
+
 
 interface FormData {
   title: string;
   description: string;
+  objective: string;
 }
 
-const Client_Form: React.FC = () => {
+const ClientForm: React.FC = () => {
   const [formData, setFormData] = useState<FormData>({
     title: '',
     description: '',
+    objective: '',
   });
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -26,34 +39,67 @@ const Client_Form: React.FC = () => {
     console.log(formData);
     // Aqui vou adicionar o controle dos dados. A rota talvez ?
   };
+  
+  const { definePartsStyle, defineMultiStyleConfig } = createMultiStyleConfigHelpers(cardAnatomy.keys)
+
+  const sizes = {
+    xl: definePartsStyle({
+      container: {
+        borderRadius: "36px",
+        padding: "40px"
+      }
+    })
+  };
 
   return (
-    
-    <Box maxW="md" mx="auto" p={4}>
-      <link rel="stylesheet" href="ClientForm.css"></link>
-      <form onSubmit={handleSubmit}>
-        <FormControl id="title" mb={4}>
-          <FormLabel>Título</FormLabel>
-          <Input
-            type="text"
-            title="title"
-            value={formData.title}
-            onChange={handleChange}
-          />
-        </FormControl>
-        <FormControl id="description" mb={4}>
-          <FormLabel>Descrição</FormLabel>
-          <Input
-            type="text"
-            title="description"
-            value={formData.description}
-            onChange={handleChange}
-          />
-        </FormControl>
-        <button className="button" type="submit">Enviar</button>
+    <Card  w="70%" h="100%">
+      <Box alignContent="left" bg="#333">
+        <CardHeader>
+          <Heading color="#ffffff" size='md'>
+          CRIAÇÃO DE PROCESSOS
+          </Heading>
+        </CardHeader>
+      </Box>
+      <CardBody>
+      <Box maxW="md" mx="auto" p={4}>
+        <form onSubmit={handleSubmit}>
+          <FormControl id="title" mb={4}>
+            <FormLabel>Título</FormLabel>
+            <Input
+              type="text"
+              title="title"
+              value={formData.title}
+              onChange={handleChange}
+            />
+          </FormControl>
+          <FormControl id="description" mb={4}>
+            <FormLabel>Descrição</FormLabel>
+            <Input
+              type="text"
+              title="description"
+              value={formData.description}
+              onChange={handleChange}
+            />
+          </FormControl>
+          <FormControl id="objective" mb={4}>
+            <FormLabel>Objetivo</FormLabel>
+            <Input
+              type="text"
+              title="objective"
+              value={formData.objective}
+              onChange={handleChange}
+            />
+          </FormControl>
+          <div>
+            <Button bg="#333" color="#ffffff" type="submit">Enviar</Button>
+          </div>
       </form>
     </Box>
+    </CardBody>
+    </Card>
   );
 };
 
-export default Client_Form;
+
+export default ClientForm;
+
