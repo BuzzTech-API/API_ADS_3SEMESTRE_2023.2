c1e44d8bcf6c27d7696490328261087857233ef4
KhovetS2 <isaque.elis.escola@gmail.com>
2023-09-17 01:07:04 -0300

    Terminando tela home

--- src/frontend/my-app/src/index.tsx
+++ src/frontend/my-app/src/index.tsx
@@ -4,15 +4,80 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ChakraProvider } from '@chakra-ui/react'
+import { createBrowserRouter, createRoutesFromElements, redirect, Route, RouterProvider } from 'react-router-dom'
+
+// layouts e paginas
+import { Home } from './pages/Home'
+import { Login } from './pages/Login'
+import SideBar from './components/SideBar/SideBar'
+import { PageModal } from './pages/ModalPage'
+import { ModalSolicitaEvidencia } from './components/Modal/BtnPedirEvidencia'
+import { ModalUploadEvidence } from './components/UploadEvidence'
+import { Authenticated, verifyToken } from './services/token'
 
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+// rotas
+const loader = () => {
+  let authenticated = new Authenticated()
+  return verifyToken(authenticated).then(() => {
+
+      if (authenticated.isAuthenticated === false) {
+          return redirect('/login')
+      } else {
+          return null
+      }
+  }).catch(() => {
+      return null
+  })
+}
+const loaderLogin = () => {
+  let authenticated = new Authenticated()
+  return verifyToken(authenticated).then(() => {
+
+
+      if (authenticated.isAuthenticated === true) {
+          return redirect('/')
+      } else {
+          return null
+      }
+  }).catch(() => {
+      return null
+  })
+}
+
+const router = createBrowserRouter(
+  [
+      {
+          path: '/',
+          loader: loader,
+          element: <App />,
+          children: [
+              {
+                  path: '/',
+                  element: <Home />
+              },
+              {
+                  path: '/modal2',
+                  element: <ModalSolicitaEvidencia />
+              }
+          ]
+      },
+      {
+          path: '/login',
+          loader: loaderLogin,
+          element: <Login />
+      }
+  ]
+)
+
 root.render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>
 );
--- src/frontend/my-app/src/pages/Home.tsx
+++ src/frontend/my-app/src/pages/Home.tsx
@@ -1,23 +1,86 @@
-import { Box, Button } from "@chakra-ui/react"
-import React from "react"
-import SideBar from "../components/SideBar/SideBar"
-import EtapaForm from "../components/EtapaForm"
-import { Outlet } from "react-router-dom"
-import { Flex} from "@chakra-ui/react"
+import React, { useEffect, useState } from "react"
+import { Box, Center, Flex, Grid, IconButton} from "@chakra-ui/react"
+import Process from "../models/Process"
+import { ProcessInterface } from "../interfaces/processInterface"
+import User from "../models/User"
+import { CardProcess } from "../components/Card/cardProcesso"
+import { CardBase } from "../components/Card/cardBase"
+import { AddIcon } from "@chakra-ui/icons"
 
 export const Home = () => {
+    const [processes, setProcesses] = useState(new Array<Process>())
+    useEffect(() => {
+        (async () => {
+            const token = localStorage.getItem('access_token');
+            const response = await fetch('http://localhost:8000/processes', {
+                method: 'GET',
+                headers: {
+                  'Accept': 'application/json',
+                  'Authorization': `Bearer ${token}`,
+                }
+              })
+
+              if (response.ok) {
+                const content = await response.json()
+                console.log(content);
+                
+                const processList= new Array<Process>()
+                content.forEach( (item: ProcessInterface) => {
+                    const usersList= new Array<User>()
+                    item.users.forEach(element => {
+                        usersList.push(element.user)
+                    });
+                    processList.push(new Process(
+                        item.id,
+                        item.title,
+                        item.endingDate,
+                        item.createDate,
+                        item.lastUpdate,
+                        item.is_active,
+                        item.priority,
+                        item.status,
+                        item.steps,
+                        usersList
+                    )
+                        )
+                        
+                });
+                setProcesses(processList)
+            }else{
+                
+              }
+              
+            })();
+        }, [])
 
     return(<div>
 
-        <Flex>
-            <Box p='2' padding='0'>
-                <SideBar></SideBar>
-            </Box>
-            <Box p='2'>
-                <Outlet />
-                <EtapaForm></EtapaForm>
-            </Box>
-        </Flex>
+        <Grid marginLeft='1rem' templateColumns='repeat(4, 1fr)' gap='1.5rem' >
+            {processes.map((process:Process) =>{
+                return <CardProcess 
+                process={process}                
+                />
+                
+
+            })}
+            <CardBase>
+                <Box padding='0' width='100%' height='100%'>
+                    <Center>
+                        <IconButton margin='50% auto'
+                                    aria-label="Btn Add Processo"
+                                    bg="#58595B"
+                                    color="white"
+                                    size='14'
+                                    icon={<AddIcon h='14' w='14' />}
+                                    _hover={{ color: "#58595B", bg: "white" }}
+                                    >
+                        </IconButton>
+                    </Center>
+                    
+                </Box>
+            </CardBase>
+            
+        </Grid>
     </div>
 
     )
