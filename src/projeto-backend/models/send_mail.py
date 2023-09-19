6f5adc80fcab23448bd8cfcb18bb793d9efc237b
Cauan Almeida <cauan.almeida@fatec.sp.gov.br>
2023-09-19 14:30:56 -0300

    Arrumando e finalizando a função de enviar emails

--- src/projeto-backend/models/send_mail.py
+++ src/projeto-backend/models/send_mail.py
@@ -14,10 +14,11 @@ credentials = dotenv_values("./.env")
 class EmailSchema(BaseModel):
     email: List[EmailStr]
 
+content = {"Email enviado com sucesso!"}
 
 conf = ConnectionConfig(
     MAIL_USERNAME = credentials['EMAIL'],
-    MAIL_PASSWORD = credentials['PASS'],
+    MAIL_PASSWORD = credentials['PASS'], #Senha de app dentro de Segurança/Verificação em duas etapas
     MAIL_FROM = credentials['EMAIL'],
     MAIL_PORT = 465,
     MAIL_SERVER = "smtp.gmail.com",
--- src/projeto-backend/routers/evidence.py
+++ src/projeto-backend/routers/evidence.py
@@ -1,18 +1,12 @@
-import json
-from fastapi import APIRouter, Depends, File, Form, HTTPException, UploadFile
+from fastapi import APIRouter, Depends, File, HTTPException, UploadFile
 from database import schemas
 from sqlalchemy.orm import Session
 from models import evidence_crud, oauth2, gcs, send_mail
 from database.database import get_db
 from typing import Annotated, Optional
 
-# mail
-from typing import List
-
-from fastapi import BackgroundTasks, FastAPI
-from fastapi_mail import ConnectionConfig, FastMail, MessageSchema, MessageType
-from pydantic import BaseModel, EmailStr
-from starlette.responses import JSONResponse
+#email
+from fastapi_mail import FastMail, MessageSchema, MessageType
 
 
 router = APIRouter(tags=["Evidences"])
@@ -75,32 +69,38 @@ async def create_upload_file(
     current_user: Annotated[schemas.User, Depends(oauth2.get_current_user)],
     emails: str,
     file: UploadFile = File(...)
-):
+): 
     """Rota para fazer upload de algum arquivo"""
-    link = await gcs.GCStorage().upload_file(file) #chama a função que o upload do arquivo para a nuvem
 
+    try:
+        
+        link = await gcs.GCStorage().upload_file(file) #chama a função que o upload do arquivo para a nuvem
+
+        
+        lista_emails = emails.split('&') #pega a string vindo do frontend, separa os emails e adciona em uma lista
+        send_mail.EmailSchema = lista_emails
+        
+        # mensagem principal que vai chegar no email dos responsáveis
+        html = """
+        <h5>Processo</h5>
+        <br>
+        <h5>Dados da evidencia</h5>
+        """ 
+
+        message = MessageSchema( #conteudos da mensagem
+            subject="Evidencias", #titulo do email
+            recipients=send_mail.EmailSchema, #emails
+            attachments=[file], #arquivos
+            body=html, #mensagem principal
+            subtype=MessageType.html)
+
+        fm = FastMail(send_mail.conf) #função que envia os emails
+        await fm.send_message(message)
     
-    lista_emails = emails.split('&') #pega a string vindo do frontend, separa os emails e adciona em uma lista
-    send_mail.EmailSchema = lista_emails
 
-    # mensagem principal que vai chegar no email dos responsáveis
-    html = """
-    <h5>Processo</h5>
-    <br>
-    <h5>Dados da evidencia</h5>
-    """ 
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    
 
-
-    message = MessageSchema( #conteudos da mensagem
-        subject="Fastapi-Mail module", #
-        recipients=send_mail.EmailSchema, #emails
-        attachments=[file], #arquivos
-        body=html, #mensagem principal
-        subtype=MessageType.html)
-
-    fm = FastMail() #função que envia os emails
-    await fm.send_message(message)
-
-    if JSONResponse(status_code=200): #retorna o link e uma mensagem avisando que o email foi enviado
-        return link, {"email foi enviado"}
+    return link, send_mail.content #retorna o link e uma mensagem avisando que o email foi enviado
     
