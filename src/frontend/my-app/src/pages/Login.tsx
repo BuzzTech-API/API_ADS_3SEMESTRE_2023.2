637e8c63224ebae72e9cce830ce6e1ae172a8ed1
KhovetS2 <isaque.elis.escola@gmail.com>
2023-09-18 23:52:50 -0300

    arrumando um pouco a tela de login

--- src/frontend/my-app/src/pages/Login.tsx
+++ src/frontend/my-app/src/pages/Login.tsx
@@ -1,71 +1,68 @@
-import { 
+import {
     Input,
     InputGroup,
     InputRightElement,
-    Button, 
+    Button,
     Box,
     Card,
     FormControl,
     FormLabel,
     Center,
-} from '@chakra-ui/react'
-import {useState} from "react";
+    Heading
 
-export const Login= () => {
+} from '@chakra-ui/react'
+import { useState } from "react";
+import { loginToken } from "./../services/token";
+
+export const Login = () => {
     const [show, setShow] = useState(false)
     const [email, setEmail] = useState('')
     const [senha, setSenha] = useState('')
     const handleClick = () => setShow(!show)
-    
-    const submit = async (e:any) => {
-        e.preventDefault();
-        
-        const response = await fetch('http://localhost:8000/login', {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            body: 'grant_type=&username='+email+'&password='+senha+'&scope=&client_id=&client_secret='
 
-        });
-        const data = await response.json()
-        localStorage.setItem('access_token',data.access_token)
-        localStorage.setItem('refresh_token',data.refresh_token)
-        
+    const submit = async (e: any) => {
+        e.preventDefault();
+
+        loginToken(email, senha)
+
     }
 
-    return <Card backgroundColor={'#292a2d'} margin={'10rem 25rem'} textColor={'white'}>
+    return <Card backgroundColor={'#58595B'} margin={'10rem auto'} textColor={'white'} width='40rem'>
         <form onSubmit={submit}>
-
-        <Box width={'20rem'} height={'30rem'} margin={'1rem auto'} padding={'1rem'}> 
-            <FormControl margin={'1rem auto'}>
-                <FormLabel textAlign={'center'}>Email</FormLabel>
-                <Input 
-                type='email' 
-                onChange={e => setEmail(e.target.value)} 
-                />
-            </FormControl>
-            <FormControl margin={'1rem auto'}>
-                <FormLabel textAlign={'center'}>Senha</FormLabel>
-                <InputGroup size='md'>
-                    <Input
-                    pr='8.5rem'
-                    type={show ? 'text' : 'password'}
-                    placeholder='Enter password'
-                    onChange={e => setSenha(e.target.value)}
-                    />
-                    <InputRightElement width='4.5rem'>
-                        <Button textColor={'white'} backgroundColor={'#4fb3bb'} h='1.75rem' size='sm' onClick={handleClick}>
-                            {show ? 'Hide' : 'Show'}
-                        </Button>
-                    </InputRightElement>
-                </InputGroup>
-            </FormControl>
-            <Center>
-                <Button type='submit' textColor={'white'} backgroundColor={'#4fb3bb'}>Logar</Button>
+            <Center padding='2rem'>
+                <Heading>Login</Heading>
             </Center>
-        </Box>
+            <Box width={'30rem'} height={'30rem'} margin={'1rem auto'} padding={'1rem'}>
+                <Center>
+
+                <FormControl margin={'1rem'}>
+                    <FormLabel textAlign={'center'}>Email</FormLabel>
+                    <Input
+                        type='email'
+                        onChange={e => setEmail(e.target.value)}
+                        />
+                </FormControl>
+                </Center>
+                <FormControl margin={'1rem auto'} width='26rem'>
+                    <FormLabel textAlign={'center'}>Senha</FormLabel>
+                    <InputGroup size='md'>
+                        <Input
+                            pr='8.5rem'
+                            type={show ? 'text' : 'password'}
+                            placeholder='Enter password'
+                            onChange={e => setSenha(e.target.value)}
+                        />
+                        <InputRightElement width='4.5rem'>
+                            <Button textColor={'white'} backgroundColor={'#4fb3bb'} h='1.75rem' size='sm' onClick={handleClick}>
+                                {show ? 'Hide' : 'Show'}
+                            </Button>
+                        </InputRightElement>
+                    </InputGroup>
+                </FormControl>
+                <Center>
+                    <Button type='submit' textColor={'white'} backgroundColor={'#4fb3bb'}>Logar</Button>
+                </Center>
+            </Box>
         </form>
     </Card>
 }
